b77b417e3fdac0dd3f0a56c4adfbaf8193f0ea19
bhawna <123251729+bhawnachandla@users.noreply.github.com>
2024-07-12 22:35:48 +0530

    my code

---
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:bhawnachandla28@gmail.com">bhawnachandla28@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/bhawna-chandla-35ab38239/">linkedin.com/bhawna</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/bhawnachandla">github.com/bhawna</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Bhawna</h1>
+        <p className={styles.description}>
+          I'm a Web developer <br />
+          with a passion for building <br />
+          beautiful and functional <br /> 
+          web applications.
+          <br />
+          Connect with me by clicking below!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/my-img.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
